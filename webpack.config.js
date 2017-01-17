7359463f7de11dfa99ad78eee7ccd5896f9ffd9d
Fengming Sun <sunfengming@meituan.com>
2017-01-17 12:21:08 +0800

    fix

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+    entry: {
+        demo: './demo/index.ts'
+    },
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: '[name].bundle.js',
+        publicPath: ''
+    },
+    resolve: {
+        extensions: ['', '.ts', '.js']
+    },
+    plugins: [new webpack.optimize.UglifyJsPlugin({
+        compress: {
+            warnings: false
+        }
+    })],
+    module: {
+        loaders: [{
+            test: /\.ts$/,
+            loader: 'ts',
+            exclude: /node_modules/
+        }, {
+            test: /\.css$/,
+            loader: 'style!css'
+        }]
+    }
+};
